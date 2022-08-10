a78eb2d1cdffc08f4e940339822d0600726936dd
Studio Kaori <studiokaori.com@gmail.com>
2022-08-10 16:42:33 +0200

    Add: AddTodoView

---
+++ SwiftUITodo/Views/AddTodoView.swift
@@ -0,0 +1,26 @@
+//
+//  AddTodoView.swift
+//  SwiftUITodo
+//
+//  Created by Kaori Persson on 2022-08-10.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }  //: Navigation
+    }
+}
+
+// MARK: - Preview
+struct AddTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTodoView()
+    }
+}
