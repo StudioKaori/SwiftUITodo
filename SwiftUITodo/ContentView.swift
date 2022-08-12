a9f0b82b12a7334a9db7c14bf1ed20c2191adfb4
Studio Kaori <studiokaori.com@gmail.com>
2022-08-12 15:58:04 +0200

    Add: ContentView

---
+++ SwiftUITodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITodo
+//
+//  Created by Kaori Persson on 2022-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
